270d0fe33e0b6bef044e93de67296f45eee7ec8e
Alena <lensa2004@inbox.ru>
2021-10-07 18:53:25 +0700

    PyCharm добавил несколько файлов

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # 1 Вебинар.
-# 1 задание
-name = input('Введите Ваше имя: ')
-last_name = input('Введите фамилию: ')
-age = int(input('Введите Ваш возраст: '))
-weight = int(input('Введите Ваш вес: '))
-first_letter_of_name = name[0].upper()
+# 2 задание
+seconds_in = int(input('Введите время в секундах: ', ))
 
-print(f'Привет, {name} {last_name}! Для своих {age} лет вы отлично выглядите, ваш вес {weight} кг идеален.')
-print(f'Первая буква вашего имени: {first_letter_of_name}')
-print('Если ваш возраст поделить на вес, получится коэффициент: {:.3f}'.format(age/weight))
-print('Тип этого коэффициента - {}'.format(type(age/weight)))
+hours = seconds_in//3600
+minutes = (seconds_in - hours*3600)/60
+seconds = (seconds_in - hours*3600) - (minutes//1)*60
+print(f'Это {hours} час(-а/-ов)', '{:.0f}'.format(minutes), 'минут(-ы)', '{:.0f}'.format(seconds), 'секунд(-ы).')
 
