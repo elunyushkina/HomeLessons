280d4607e82ae5c18cac7b7e599b0bb6cf86d2b6
Alena <lensa2004@inbox.ru>
2021-10-07 18:54:42 +0700

    PyCharm добавил несколько файлов

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # 1 Вебинар.
-# 3 задание
-n = input('Введите число n: ', )
-nn = n + n
-nnn = n + nn
-print('Конкатенация введенного числа: ', n, nn, nnn)
-summa = int(n) + int(nn) + int(nnn)
-print('Сумма конкатенированных значений от введенного числа: ', summa)
+# 4 задание
+number = input('Введите целое положительное число: ', )
+max_num = 0
+n = 0
+while n != len(number):
+    x = int(number[n])
+    if max_num < x:
+        max_num = x
+    n +=1
+else:
+    print(f'Самая большая цифра в числе = {max_num}')
 
