52207aa099b6e7d39d63e66383444b9ff5edaa61
Alena <lensa2004@inbox.ru>
2021-10-07 18:56:08 +0700

    PyCharm добавил несколько файлов

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# 1 Урок.
-# 5 задание
-gain = int(input('Укажите выручку фирмы: ', ))
-outgoing = int(input('Укажите издержки фирмы: ', ))
-if gain > outgoing:
-    profit = gain - outgoing
-    rentab = profit / gain
-    print(f'Ваша фирма работает с прибылью {profit}, и с рентабельностью = ', '{:.3f}'.format(rentab), ', поздравляем.')
-    members = int(input('Введите число сотрудников фирмы: ' ))
-    profit_1member = profit / members
-    print(f'Прибыль фирмы в рассчете на одного человека составила: ', '{:.3f}'.format(profit_1member))
+# 1 Вебинар.
+# 6 задание
+a = int(input('Введите пробег 1-го дня в км: ', ))
+b = int(input('Введите пробег последнего дня в км: ', ))
+n = 1
+while a <= b:
+    print(f'{n}-й день: {a}')
+    a = a + round((a*(10)**-1), 2)
+    a = round(a, 2)
+    n +=1
 else:
-    print('Фирма работает в убыток, сожалеем.')
+    print(f'{n}-й день: {a}')
+    print(f'На {n}-й день спортсмен достиг результата  - не менее {b} км')
 
